fea234f1d877a6e1db23f9c0295030074cf7e912
Amit Das <amit.das@iserveu.co.in>
2025-08-11 17:12:59 +0530

    a

---
+++ app/services/security-compliance/page.tsx
@@ -0,0 +1,7 @@
+export default function SecurityCompliance() {
+  return (
+    <div className="flex items-center top-50 justify-center h-screen bg-gray-900">
+        <h1 className="text-7xl mt-50 font-extrabold">Coming Soon !!!</h1>
+    </div>
+  )
+}
